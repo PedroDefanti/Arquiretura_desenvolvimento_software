df21bbb92964667afc6fb8e427518f198c3ff346
Pedro  Defanti <168847376+PedroDefanti@users.noreply.github.com>
2026-08-24 20:05:32 -0300

    exercicio 01 de html

---
+++ 24/08/EX01_HTML.py
@@ -0,0 +1,140 @@
+from abc import ABC, abstractmethod
+
+
+
+class ElementoHTML(ABC):
+    @abstractmethod
+    def renderizar(self):
+        pass
+
+
+class Paragrafo(ElementoHTML):
+    pass
+
+
+class Tabela(ElementoHTML):
+    pass
+
+
+class Imagem(ElementoHTML):
+    pass
+
+
+class Link(ElementoHTML):
+    pass
+
+
+class Formulario(ElementoHTML):
+    pass
+
+
+
+class ParagrafoHTML5(Paragrafo):
+    def __init__(self, texto):
+        self.texto = texto
+
+    def renderizar(self):
+        return f"<p>{self.texto}</p>"
+
+
+class TabelaHTML5(Tabela):
+    def renderizar(self):
+        return """<table>
+    <tr>
+        <th>Nome</th>
+        <th>Idade</th>
+    </tr>
+    <tr>
+        <td>João</td>
+        <td>20</td>
+    </tr>
+</table>"""
+
+
+class ImagemHTML5(Imagem):
+    def __init__(self, url, texto_alternativo):
+        self.url = url
+        self.texto_alternativo = texto_alternativo
+
+    def renderizar(self):
+        return f'<img src="{self.url}" alt="{self.texto_alternativo}">'
+
+
+class LinkHTML5(Link):
+    def __init__(self, url, texto):
+        self.url = url
+        self.texto = texto
+
+    def renderizar(self):
+        return f'<a href="{self.url}">{self.texto}</a>'
+
+
+class FormularioHTML5(Formulario):
+    def renderizar(self):
+        return """<form>
+    <label>Nome:</label>
+    <input type="text" name="nome">
+    <button type="submit">Enviar</button>
+</form>"""
+
+
+
+class FabricaHTML(ABC):
+    @abstractmethod
+    def criar_paragrafo(self):
+        pass
+
+    @abstractmethod
+    def criar_tabela(self):
+        pass
+
+    @abstractmethod
+    def criar_imagem(self):
+        pass
+
+    @abstractmethod
+    def criar_link(self):
+        pass
+
+    @abstractmethod
+    def criar_formulario(self):
+        pass
+
+
+
+class FabricaHTML5(FabricaHTML):
+    def criar_paragrafo(self):
+        return ParagrafoHTML5("Este é um parágrafo HTML.")
+
+    def criar_tabela(self):
+        return TabelaHTML5()
+
+    def criar_imagem(self):
+        return ImagemHTML5("https://exemplo.com/imagem.jpg", "Imagem de exemplo")
+
+    def criar_link(self):
+        return LinkHTML5("https://www.google.com", "Acessar página")
+
+    def criar_formulario(self):
+        return FormularioHTML5()
+
+
+
+def main():
+    fabrica = FabricaHTML5()
+
+    paragrafo = fabrica.criar_paragrafo()
+    tabela = fabrica.criar_tabela()
+    imagem = fabrica.criar_imagem()
+    link = fabrica.criar_link()
+    formulario = fabrica.criar_formulario()
+
+    print(paragrafo.renderizar())
+    print(tabela.renderizar())
+    print(imagem.renderizar())
+    print(link.renderizar())
+    print(formulario.renderizar())
+
+
+if __name__ == "__main__":
+    main()
